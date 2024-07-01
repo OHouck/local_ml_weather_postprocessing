d93984907b55b37e8ce08022b3d1cac82ec4a858
OHouck <ozhouck@gmail.com>
2024-07-01 17:13:05 -0500

    trash commit before pulling from anvil

--- weatherbench2_eval.py
+++ weatherbench2_eval.py
@@ -34,7 +34,7 @@ def eval_forecast(forecast_name, forecast_path, obs_path, climatology_path,
         f'--climatology_path={climatology_path}',
         f'--output_dir={output_dir}',
         f'--output_file_prefix={forecast_name}_{region}_',
-        '--input_chunks=init_time=1',
+        '--input_chunks=init_time=1,lead_time=1',
         '--eval_configs=deterministic',
         f'--time_start={time_start}',
         f'--time_stop={time_stop}',
@@ -43,38 +43,37 @@ def eval_forecast(forecast_name, forecast_path, obs_path, climatology_path,
         '--use_beam=True',
         '--runner=DirectRunner',
         '--',
-        '--direct_num_workers=4'
+        '--direct_num_workers=2'
     ]
     subprocess.run(cmd, check=True)
 
-#output_dir = "/Users/ohouck/Library/CloudStorage/OneDrive-TheUniversityofChicago/ai_weather_ag/forecasts/weatherbench2_output"
-#code_dir = "/Users/ohouck/vc/ai_weather_ag/"
+output_dir = "/Users/ohouck/Library/CloudStorage/OneDrive-TheUniversityofChicago/ai_weather_ag/forecasts/weatherbench2_output"
+code_dir = "/Users/ohouck/vc/ai_weather_ag/"
 
-output_dir = "/anvil/projects/x-atm170020/ohouck/output/weatherbench2"
-code_dir = "/anvil/projects/x-atm170020/ohouck/ai_weather_ag"
+# output_dir = "/anvil/projects/x-atm170020/ohouck/output/weatherbench2"
+# code_dir = "/anvil/projects/x-atm170020/ohouck/ai_weather_ag"
 # list of supported regions in evaluate.py
-region = "global"
+region = "pakistan"
 
+# coarse resolution for testing
+era5_64x32 = 'gs://weatherbench2/datasets/era5/1959-2023_01_10-6h-64x32_equiangular_conservative.zarr'
+era5_64x32_climatology = 'gs://weatherbench2/datasets/era5-hourly-climatology/1990-2019_6h_64x32_equiangular_conservative.zarr'
+pangu_test_path = 'gs://weatherbench2/datasets/pangu/2018-2022_0012_64x32_equiangular_conservative.zarr'
+pangu_test_vars = 'temperature'
 time_start = '2020-01-01'
 time_stop= '2020-07-01'
 
-## coarse resolution for testing
-#era5_64x32 = 'gs://weatherbench2/datasets/era5/1959-2023_01_10-6h-64x32_equiangular_conservative.zarr'
-#era5_64x32_climatology = 'gs://weatherbench2/datasets/era5-hourly-climatology/1990-2019_6h_64x32_equiangular_conservative.zarr'
-#pangu_test_path = 'gs://weatherbench2/datasets/pangu/2018-2022_0012_64x32_equiangular_conservative.zarr'
-#pangu_test_vars = 'temperature'
-#
-## start timer
-#test_start = time.time()
-## test pangu
-#eval_forecast(forecast_name = 'pangu',forecast_path = pangu_test_path, obs_path = era5_64x32, 
-             #climatology_path = era5_64x32_climatology, variables = pangu_test_vars,
-             #region = region, time_start=time_start, time_stop=time_stop)
-#test_end = time.time()
-## time elapsed in hours and minutes
-#test_elapsed = test_end - test_start
-#print(f"Test elapsed time: {test_elapsed/3600} hours")
-#exit()
+# start timer
+test_start = time.time()
+# test pangu
+eval_forecast(forecast_name = 'pangu',forecast_path = pangu_test_path, obs_path = era5_64x32, 
+             climatology_path = era5_64x32_climatology, variables = pangu_test_vars,
+             region = region, time_start=time_start, time_stop=time_stop)
+test_end = time.time()
+# time elapsed in hours and minutes
+test_elapsed = test_end - test_start
+print(f"Test elapsed time: {test_elapsed/3600} hours")
+exit()
 
 # 0.25 degree resolution
 era5_1440x721_path = 'gs://weatherbench2/datasets/era5/1959-2023_01_10-wb13-6h-1440x721_with_derived_variables.zarr'
@@ -103,7 +102,7 @@ neural_gcm_deterministic_path = 'gs://weatherbench2/datasets/neuralgcm_determini
 neural_gcm_deterministic_vars = 'temperature, P_minus_E_cumulative'
 
 file = open('run_times.txt', 'w')
-output = "Start recording run times :)"
+output = "start tracking run times :}"
 file.write(output)
 file.close()
 
@@ -112,7 +111,7 @@ file.close()
 pangu_start = time.time()
 eval_forecast(forecast_name = 'pangu',forecast_path = pangu_path, obs_path = era5_1440x721_path, 
               climatology_path = era5_1440x721_climatology_path, variables = pangu_vars,
-              region = region, time_start = time_start, time_stop=time_stop)
+              region = region)
 pangu_end = time.time()
 pangu_time = str((pangu_end - pangu_start) / 3600)
 
@@ -121,28 +120,28 @@ output = "pangu time: " + pangu_time
 file.write(output)
 file.close()
 
-## ifs 
-#ifs_start = time.time()
-#eval_forecast(forecast_name = 'ifs_hres',forecast_path = ifs_hres_path, obs_path = era5_1440x721_path, 
-              #climatology_path = era5_1440x721_climatology_path, variables = ifs_vars,
-              #region = region, time_start = time_start, time_stop=time_stop)
-#ifs_end = time.time()
-#ifs_time = str((ifs_end - ifs_start) / 3600)
-#
-#file = open(code_dir + "run_times.txt", "w")
-#output = "ifs time: " + ifs_time 
-#file.write(output)
-#file.close()
-#
-## neural_gcm
-#neural_start = time.time()
-#eval_forecast(forecast_name = 'neural_gcm_deterministic',forecast_path = neural_gcm_deterministic_path, obs_path =era5_240x121_path, 
-              #climatology_path =era_240x121_climatology_path, variables = neural_gcm_deterministic_vars,
-              #region = region, time_start = time_start, time_stop=time_stop)
-#neural_end= time.time()
-#neural_time = str((neural_end - neural_start) / 3600)
-#
-#file = open(code_dir + "run_times.txt", "w")
-#output = "neural gcm time: " + neural_time 
-#file.write(output)
-#file.close()
+# ifs 
+ifs_start = time.time()
+eval_forecast(forecast_name = 'ifs_hres',forecast_path = ifs_hres_path, obs_path = era5_1440x721_path, 
+              climatology_path = era5_1440x721_climatology_path, variables = ifs_vars,
+              region = region)
+ifs_end = time.time()
+ifs_time = str((ifs_end - ifs_start) / 3600)
+
+file = open(code_dir + "run_times.txt", "w")
+output = "ifs time: " + ifs_time 
+file.write(output)
+file.close()
+
+# neural_gcm
+neural_start = time.time()
+eval_forecast(forecast_name = 'neural_gcm_deterministic',forecast_path = neural_gcm_deterministic_path, obs_path =era5_240x121_path, 
+              climatology_path =era_240x121_climatology_path, variables = neural_gcm_deterministic_vars,
+              region = region)
+neural_end= time.time()
+neural_time = str((neural_end - neural_start) / 3600)
+
+file = open(code_dir + "run_times.txt", "w")
+output = "neural gcm time: " + neural_time 
+file.write(output)
+file.close()
