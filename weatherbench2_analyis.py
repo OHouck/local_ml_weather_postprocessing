2f9fe2ed22b29f6849361ec125388d2760362edb
OHouck <ozhouck@gmail.com>
2024-07-25 17:08:59 -0500

    fixed weatherbench figs

--- weatherbench2_analyis.py
+++ weatherbench2_analyis.py
@@ -8,9 +8,12 @@
 import xarray as xr
 import matplotlib.pyplot as plt
 
+# local
 output_dir = "/Users/ohouck/Library/CloudStorage/OneDrive-TheUniversityofChicago/ai_weather_ag/forecasts/weatherbench2"
+fig_dir = "/Users/ohouck/Library/CloudStorage/OneDrive-TheUniversityofChicago/ai_weather_ag/figures/weatherbench2"
 code_dir = "/Users/ohouck/vc/ai_weather_ag/"
 
+# anvil
 # output_dir = "/anvil/projects/x-atm170020/ohouck/output/weatherbench2"
 # code_dir = "/anvil/projects/x-atm170020/ohouck/ai_weather_ag"
 
@@ -18,41 +21,32 @@ region = "pakistan"
 
 # Evaluations created by weatherbench2
 pangu = xr.open_dataset(f'{output_dir}/pangu_{region}_deterministic.nc')
+# pangu['lead_time'] = (pangu['lead_time'] * 2.77778e-13) / 24 # convert lead time to days (first to hours, then to days)
+pangu['lead_time'] = pangu['lead_time'].astype(float) * 1.15741e-14 
+print(pangu)
 
 ifs = xr.open_dataset(f'{output_dir}/ifs_hres_{region}_deterministic.nc')
+ifs['lead_time'] = ifs['lead_time'].astype(float) * 1.15741e-14
 
 neural_gcm = xr.open_dataset(f'{output_dir}/neural_gcm_{region}_deterministic.nc')
+neural_gcm['lead_time'] = neural_gcm['lead_time'].astype(float) * 1.15741e-14
 
-# rename variables to merge 
-pangu = pangu.rename_vars({'temperature': 'pangu_temperature', 
-                           '2m_temperature': 'pangu_2m_temperature'})
-ifs = ifs.rename_vars({'temperature': 'ifs_temperature',
-                     '2m_temperature': 'ifs_2m_temperature',
-                     'total_precipitation_24hr': 'ifs_total_precipitation_24hr'})
-neural_gcm = neural_gcm.rename_vars({'temperature': 'neural_gcm_temperature'})
-
-# merge datasets
-combined = xr.merge([pangu, ifs, neural_gcm])
-
-# convert lead time from nano seconds to days
-combined['lead_time'] = combined.lead_time * 1.15741e-14
-
-print(combined['neural_gcm_temperature'].sel(level=850).sel(metric='acc'))
-
-# make plot coming temperature at 850 hPa
-combined['pangu_temperature'].sel(level=850).sel(metric='mse').plot(label='pangu', color='lightgreen')
-combined['ifs_temperature'].sel(level=850).sel(metric='mse').plot(label='ifs', color='blue')
-combined['neural_gcm_temperature'].sel(level=850).sel(metric='mse').plot(label='neural_gcm', color='darkgreen')
+pangu.temperature.sel(level=850).sel(metric='mse').plot(label='Pangu', color='lightgreen')
+ifs.temperature.sel(level=850).sel(metric='mse').plot(label='IFS', color='blue')
+neural_gcm.temperature.sel(level=850).sel(metric='mse').plot(label='Neural GCM', color='darkgreen')
 plt.legend()
 plt.ylabel('MSE')
-plt.title('Temperature at 850 hPa')
-plt.show()
-
-combined['pangu_2m_temperature'].sel(metric='mse').plot(label='pangu', color='lightgreen')
-combined['ifs_2m_temperature'].sel(metric='mse').plot(label='ifs', color='forestgreen')
-plt.legend()
 plt.xlabel('Lead Time (days)')
-plt.ylabel('MSE')
-plt.title('2m Temperature MSE')
-plt.show()
+plt.title(f'{region} Temperature at 850 hPa')
+plt.savefig(f'{fig_dir}/weatherbench2_temperature_850hPa_{region}.png')
+plt.clf()
 
+# plot t2m mse for pangu and ifs (neural gcm doesn't have t2m)
+pangu['2m_temperature'].sel(metric='mse').plot(label='Pangu', color='lightgreen')
+ifs['2m_temperature'].sel(metric='mse').plot(label='IFS', color='blue')
+plt.legend()
+plt.ylabel('MSE')
+plt.xlabel('Lead Time (days)')
+plt.title(f'{region} 2m Temperature')
+plt.savefig(f'{fig_dir}/weatherbench2_t2m_{region}.png')
+plt.clf()
