dbb9c4db55adaa30184b352489d9489c83b1919c
Oskar Houck <ohouck@midway3-login4.rcc.local>
2025-11-01 11:26:03 -0500

    hyperparm tun didn't work ohwell

--- finetuning/hyperparam_tuning.sh
+++ finetuning/hyperparam_tuning.sh
@@ -5,7 +5,7 @@
 #SBATCH --output=output-%J.txt
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
-#SBATCH --time=0:10:00
+#SBATCH --time=1:00:00
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1 
  
@@ -14,9 +14,3 @@ source .venv/bin/activate
 
 uv run hyperparam_tuning.py 
  
-HOST_IP=`/sbin/ip route get 8.8.8.8 | awk '{print $7;exit}'`
-PORT_NUM=$(shuf -i15001-30000 -n1)
- 
-marimo edit --headless --host $HOST_IP --port=$PORT_NUM
-
-echo "Connect to: http://${HOST_IP}:${PORT_NUM}"
